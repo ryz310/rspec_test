d0d28cd32c42aa4dd2a5fe97abcb47b176ecb9f0
ryosuke_sato <ryz310@gmail.com>
2014-01-15 15:57:17 +0900

    models/customer の spec を追加

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,14 +2,17 @@
 require 'spec_helper'
 
 describe Customer do
-  specify "column 'family_name' should not be empty" do
-    customer = Customer.new(
-      family_name: '',
-      given_name: '太郎',
-      family_name_kana: 'ヤマダ',
-      given_name_kana: 'タロウ'
-    )
-    expect(customer).not_to be_valid
-    expect(customer.errors[:family_name]).to be_present
+  %w{family_name family_name_kana given_name given_name_kana}.each do |column_name|
+    specify "#{column_name} should not be empty" do
+      customer = Customer.new(
+        family_name: '山田',
+        given_name: '太郎',
+        family_name_kana: 'ヤマダ',
+        given_name_kana: 'タロウ'
+      )
+      customer[column_name] = ''
+      expect(customer).not_to be_valid
+      expect(customer.errors[column_name]).to be_present
+    end
   end
 end
