304b1546c869ccd273cdc6f5dfeb700b4a7180ee
ryosuke_sato <ryz310@gmail.com>
2014-01-15 18:57:53 +0900

    spec を実装

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,36 +2,27 @@
 require 'spec_helper'
 
 describe Customer do
-  specify 'valid object' do
-    customer = Customer.new(
-      family_name: '山田',
-      given_name: '太郎',
+  let(:customer) do 
+    Customer.new(
+      family_name:      '山田',
+      given_name:       '太郎',
       family_name_kana: 'ヤマダ',
-      given_name_kana: 'タロウ'
+      given_name_kana:  'タロウ'
     )
+  end
+
+  specify 'valid object' do
     expect(customer).to be_valid
   end
 
   %w{family_name family_name_kana given_name given_name_kana}.each do |column_name|
     specify "#{column_name} should not be empty" do
-      customer = Customer.new(
-        family_name: '山田',
-        given_name: '太郎',
-        family_name_kana: 'ヤマダ',
-        given_name_kana: 'タロウ'
-      )
       customer[column_name] = ''
       expect(customer).not_to be_valid
       expect(customer.errors[column_name]).to be_present
     end
 
     specify "#{column_name} should be less than 40 characters" do
-      customer = Customer.new(
-        family_name: '山田',
-        given_name: '太郎',
-        family_name_kana: 'ヤマダ',
-        given_name_kana: 'タロウ'
-      )
       customer[column_name] = "あ" * 41
       expect(customer).not_to be_valid
       expect(customer.errors[column_name]).to be_present
