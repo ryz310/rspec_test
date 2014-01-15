d9fa394796a0b7c5e2bbe71bf681216744ffeba4
ryosuke_sato <ryz310@gmail.com>
2014-01-15 17:08:57 +0900

    spec を実装

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,5 @@
 class Customer < ActiveRecord::Base
-  validates :family_name, :family_name_kana, :given_name, :given_name_kana, presence: true
+  validates :family_name, :family_name_kana, :given_name, :given_name_kana, 
+    presence: true, 
+    length:   {maximum: 40}
 end
