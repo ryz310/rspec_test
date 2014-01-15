8f36da9365ad26d216016b845ad2a3f043f2173f
ryosuke_sato <ryz310@gmail.com>
2014-01-15 15:50:16 +0900

    models/customer の spec を作成

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,15 @@
+# coding: utf-8
 require 'spec_helper'
 
 describe Customer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  specify "column 'family_name' should not be empty" do
+    customer = Customer.new(
+      family_name: '',
+      given_name: '太郎',
+      family_name_kana: 'ヤマダ',
+      given_name_kana: 'タロウ'
+    )
+    expect(customer).not_to be_valid
+    expect(customer.errors[:family_name]).to be_present
+  end
 end
