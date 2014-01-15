c49509c95a6d15620e2dfb902a0175236335e312
ryosuke_sato <ryz310@gmail.com>
2014-01-15 16:17:36 +0900

    spec を追加

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,6 +2,16 @@
 require 'spec_helper'
 
 describe Customer do
+  specify 'valid object' do
+    customer = Customer.new(
+      family_name: '山田',
+      given_name: '太郎',
+      family_name_kana: 'ヤマダ',
+      given_name_kana: 'タロウ'
+    )
+    expect(customer).to be_valid
+  end
+
   %w{family_name family_name_kana given_name given_name_kana}.each do |column_name|
     specify "#{column_name} should not be empty" do
       customer = Customer.new(
