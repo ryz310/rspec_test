eb0c7ec231fa6091b9cd412a9bbb85cc726eefa2
Ryosuke Sato <ryosuke.sato@gi-no.jp>
2014-02-06 11:31:11 +0900

    Customer をリファクタリング

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -36,19 +36,7 @@ class Customer < ActiveRecord::Base
       customer = find_by_username(username)
       if (customer.try(:password_digest) &&
           BCrypt::Password.new(customer.password_digest) == password)
-        Time.zone = 'Tokyo'
-        now = Time.current
-        if now.hour < 5
-          time0 = now.yesterday.midnight.advance(hours: 5)
-          time1 = now.midnight.advance(hours: 5)
-        else
-          time0 = now.midnight.advance(hours: 5)
-          time1 = now.tomorrow.midnight.advance(hours: 5)
-        end
-
-        unless customer.rewords.where(created_at: time0...time1).exists?
-          customer.rewords.create(points: 1)
-        end
+        RewardManager.new(customer).grant_login_points
         customer
       else
         nil
